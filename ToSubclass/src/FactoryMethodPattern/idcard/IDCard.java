c94504b1db8b727497c66582863b9dc4e11b966c
BuFanJun-WF <Fan.Wang.1984@outlook.com>
2022-04-14 09:16:43 +0800

    8.完成习题4-2

--- ToSubclass/src/FactoryMethodPattern/idcard/IDCard.java
+++ ToSubclass/src/FactoryMethodPattern/idcard/IDCard.java
@@ -11,17 +11,33 @@ import FactoryMethodPattern.framework.Product;
 public class IDCard extends Product {
     private String owner;
 
+    /**
+     * 添加IDCard类的卡的编号
+     */
+    private int serial;
+
     IDCard(String owner){
         System.out.println("制作" + owner + "的ID卡");
+        this.owner = owner;
+    }
+
+    IDCard(String owner, int serial){
+        System.out.println("制作" + owner + "(" + serial + ")" + "的ID卡");
         this.owner = owner;
     }
 
     @Override
     public void use() {
         System.out.println("使用" + owner + "的ID卡");
+        System.out.println("使用" + owner + "(" + serial + ")" + "的ID卡");
+
     }
 
     public String getOwner() {
         return owner;
+    }
+
+    public int getSerial() {
+        return serial;
     }
 }
--- ToSubclass/src/FactoryMethodPattern/idcard/IDCardFactory.java
+++ ToSubclass/src/FactoryMethodPattern/idcard/IDCardFactory.java
@@ -4,6 +4,8 @@ import FactoryMethodPattern.framework.Factory;
 import FactoryMethodPattern.framework.Product;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 
 /**
@@ -16,10 +18,10 @@ public class IDCardFactory extends Factory {
 
     private List<String> owners = new ArrayList<>();
 
-    @Override
-    protected Product createProduct(String owner) {
-        return new IDCard(owner);
-    }
+    // @Override
+    // protected Product createProduct(String owner) {
+    //     return new IDCard(owner);
+    // }
 
     @Override
     protected void registerProduct(Product product) {
@@ -28,5 +30,17 @@ public class IDCardFactory extends Factory {
 
     public List<String> getOwners() {
         return owners;
+    }
+
+    private HashMap database = new HashMap();
+    private int serial = 100;
+    @Override
+    protected synchronized Product createProduct(String owner) {
+        return new IDCard(owner, serial++);
+    }
+
+    public HashMap getDatabase() {
+        return database;
+
     }
 }
